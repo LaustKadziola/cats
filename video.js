10862bd437806d4197cb986a1db7e2213f0060c9
SpinUpCat <SpinUpCat@gmail.com>
2020-10-18 03:49:55 +0200

    improved style

--- video.js
+++ video.js
@@ -5,14 +5,14 @@ var source = document.createElement('source');
 v.appendChild(source);
 
 
-source.setAttribute('src','Striking.mp4');
+source.setAttribute('src','resources/videos/Striking.mp4');
 
 v.addEventListener(
     'timeupdate',
     function (event) {
         console.log(v.currentTime);
-        if(v.currentTime > 1){
-
+        if(v.currentTime > v.duration * 0.80){
+            console.log("times up");
         }
     },
     false
@@ -22,9 +22,9 @@ v.addEventListener(
 var button1 = document.getElementById('button_1');
 var button2 = document.getElementById('button_2');
 var button3 = document.getElementById('button_3');
-button1.addEventListener("click", function(){changeVideo('bath.mp4')});
-button2.addEventListener("click", function(){changeVideo('SQUISH.mp4')});
-button3.addEventListener("click", function(){changeVideo('Striking.mp4')});
+button1.addEventListener("click", function(){changeVideo('resources/videos/bath.mp4')});
+button2.addEventListener("click", function(){changeVideo('resources/videos/SQUISH.mp4')});
+button3.addEventListener("click", function(){changeVideo('resources/videos/Striking.mp4')});
 
 
 function changeVideo(src){
@@ -39,10 +39,16 @@ function playVideo(){
     v.play();
 }
 
-class Node {
+class Choice {
     constructor(title, video, options) {
         this.title = title;
         this.video = video;
         this.options = options;
+    }
+
+    GenerateOptions(){
+        options.forEach(option => {
+            
+        });
     }
 }
