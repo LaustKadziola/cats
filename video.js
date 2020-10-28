9268437ad4450eda8fd7ae416ccf7740abf3f4fc
SpinUpCat <SpinUpCat@gmail.com>
2020-10-28 19:48:26 +0100

    Created dynamic buttons

--- video.js
+++ video.js
@@ -4,30 +4,44 @@ var v = document.getElementsByTagName('video')[0];
 var source = document.createElement('source');
 v.appendChild(source);
 
-
+var buttons = document.getElementById('buttons');
 source.setAttribute('src','resources/videos/Striking.mp4');
 
 v.addEventListener(
     'timeupdate',
     function (event) {
         console.log(v.currentTime);
-        if(v.currentTime > v.duration * 0.80){
+        if(v.currentTime > v.duration * 0.10){
             console.log("times up");
+            buttons.style.display = "block";
         }
     },
     false
 );
 
+var buttonNames = ["bath", "SQUISH", "Striking"];
+var videoPath = "resources/videos/"
 
-var button1 = document.getElementById('button_1');
-var button2 = document.getElementById('button_2');
-var button3 = document.getElementById('button_3');
-button1.addEventListener("click", function(){changeVideo('resources/videos/bath.mp4')});
-button2.addEventListener("click", function(){changeVideo('resources/videos/SQUISH.mp4')});
-button3.addEventListener("click", function(){changeVideo('resources/videos/Striking.mp4')});
+var buttonList = document.getElementById('buttons').querySelectorAll("button");
+
+changeButtons();
+
+function changeButtons(){
+    
+    for (let i = 0; i < buttonList.length; i++) {
+        if(i<buttonNames.length){
+            buttonList[i].innerText = buttonNames[i]
+            buttonList[i].addEventListener("click", function(){changeVideo("resources/videos/" + buttonNames[i] + ".mp4")});
+        }else{
+            buttonList[i].style.display = "none";
+        } 
+
+    }
+}
 
 
 function changeVideo(src){
+    buttons.style.display = "none";
     console.log("Changing Video");
     source.setAttribute('src',src);
     v.load();
