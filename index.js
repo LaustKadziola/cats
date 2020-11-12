1eb55771de1eb40e8992e26f1b4355a5edf26a22
SpinUpCat <SpinUpCat@gmail.com>
2020-11-12 18:59:17 +0100

    made it better

--- index.js
+++ index.js
@@ -60,6 +60,6 @@ const server = http.createServer((req, res) => {
     });
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, ()=> console.log(`Server running on port ${port}`));
